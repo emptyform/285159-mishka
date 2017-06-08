0a240beb05695e36e8b85a6230027b89635ac509
Polisonik <allred@inbox.ru>
2017-06-08 20:31:01 +0300

    начало верстки адаптивной сетки index.html

--- js/open-windows.js
+++ js/open-windows.js
@@ -1,34 +1,14 @@
-var toggle = document.querySelector(".page-header__toggle");
 var menu = document.querySelector(".main-nav");
+var toggle = document.querySelector(".main-nav__toggle");
 
-menu.classList.toggle("main-nav--close");
-toggle.addEventListener("click", function (event) {
-  event.preventDefault();
-  menu.classList.toggle("main-nav--close");
-});
+menu.classList.remove("main-nav--nojs");
 
-var order = document.querySelector(".button--order");
-var popup = document.querySelector(".modal");
-var overlay = document.querySelector(".modal-overlay");
-
-order.addEventListener("click", function (event) {
-  event.preventDefault();
-  popup.classList.add("modal-show");
-  overlay.classList.add("modal-overlay-show");
-});
-
-window.addEventListener("keydown", function (event) {
-  if (event.keyCode === 27) {
-    if (popup.classList.contains("modal-show")) {
-      popup.classList.remove("modal-show");
-      overlay.classList.remove("modal-overlay-show");
-    }
-  }
-});
-
-overlay.addEventListener("click", function (event) {
-  if (popup.classList.contains("modal-show")) {
-    popup.classList.remove("modal-show");
-    overlay.classList.remove("modal-overlay-show");
+toggle.addEventListener("click", function () {
+  if (menu.classList.contains("main-nav--closed")) {
+      menu.classList.remove("main-nav--closed");
+      menu.classList.add("main-nav--opened");
+  } else {
+    menu.classList.add("main-nav--closed");
+    menu.classList.remove("main-nav--opened");
   }
 });
