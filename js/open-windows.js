27bd0485d86f108b6726e1f3f9caaf39e30e4659
Polisonik <allred@inbox.ru>
2017-06-13 20:17:15 +0300

    добавлено модальное окно

--- js/open-windows.js
+++ js/open-windows.js
@@ -1,6 +1,11 @@
 var menu = document.querySelector(".main-nav");
 var toggle = document.querySelector(".main-nav__toggle");
 
+var openModal = document.querySelector(".button--order");
+var modalWindow = document.querySelector(".modal");
+var overlay = document.querySelector(".modal-overlay");
+
+
 menu.classList.remove("main-nav--nojs");
 
 toggle.addEventListener("click", function () {
@@ -11,4 +16,26 @@ toggle.addEventListener("click", function () {
     menu.classList.add("main-nav--closed");
     menu.classList.remove("main-nav--opened");
   }
+});
+
+openModal.addEventListener("click", function (event) {
+  event.preventDefault();
+  modalWindow.classList.add("modal-show");
+  overlay.classList.add("modal-overlay-show");
+});
+
+window.addEventListener ("keydown", function (event) {
+  if (event.keyCode === 27) {
+    if (modalWindow.classList.contains("modal-show")){
+      modalWindow.classList.remove("modal-show");
+      overlay.classList.remove("modal-overlay-show");
+    }
+  }
+});
+
+overlay.addEventListener ("click", function (event) {
+  if (modalWindow.classList.contains("modal-show")){
+      modalWindow.classList.remove("modal-show");
+      overlay.classList.remove("modal-overlay-show");
+    }
 });
